dd352d1e06c7634673e40189412e9a56b64e286e
MinT <135308254+mintdunno@users.noreply.github.com>
2023-09-25 00:16:03 +0700

    Delete .history directory

--- .history/routes/register_20230921205104.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const registerRouter = express.Router();
-const schema = require("../models/schema");
-const Customer = schema.Customer;
-const Vendor = schema.Vendor;
-const Shipper = schema.Shipper;
-const bcrypt = require("bcryptjs");
